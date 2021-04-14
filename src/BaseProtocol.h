65ed773ca33860d7e101ff9c8dc67059a6a156ee
Matthijs Kooijman <matthijs@stdin.nl>
2021-04-14 11:07:44 +0200

    Update protocol code from latest bootloader
    This copies the protocol code from a more recent bootloader, which has
    been refactored to support I2C and RS485 (though no RS485 support for
    the attiny exists, so code for RS485 is partially added but incomplete
    and unused).
    
    In addition to adapting to the new code structure, this adds support for
    some of the bootloader commands (GET_PROTOCOL_VERSION, SET_ADDRESS and
    GET_MAX_PACKET_LENGTH), which makes it easier to manage multiple child
    boards from the mainboard.
    
    This also renames Hardware.h to Config.h, which is what the bootloader
    now uses for its configuration.
    
    The following files were copied from ChildbusBootloader, commit 29f1e52
    (PROTOCOL: Bump version to 2.1):
    
        cp ../../ChildbusBootloader/attiny/{TwoWire.cpp,Reset.cpp} src/attiny/
        cp ../../ChildbusBootloader/{BaseProtocol.cpp,BaseProtocol.h,Crc.h,Bus.h} src/

--- src/BaseProtocol.h
+++ src/BaseProtocol.h
@@ -18,32 +18,48 @@
 #pragma once
 
 #include <stdint.h>
+#include "Config.h"
 
 struct Status {
-  static const uint8_t COMMAND_OK            = 0x00;
-  static const uint8_t COMMAND_FAILED        = 0x01;
-  static const uint8_t COMMAND_NOT_SUPPORTED = 0x02;
-  static const uint8_t INVALID_TRANSFER      = 0x03;
-  static const uint8_t INVALID_CRC           = 0x04;
-  static const uint8_t INVALID_ARGUMENTS     = 0x05;
-  // Never sent, used internally to indicate no status should be
-  // returned.
-  static const uint8_t NO_REPLY              = 0xff;
+	static const uint8_t COMMAND_OK            = 0x00;
+	static const uint8_t COMMAND_FAILED        = 0x01;
+	static const uint8_t COMMAND_NOT_SUPPORTED = 0x02;
+	static const uint8_t INVALID_TRANSFER      = 0x03;
+	static const uint8_t INVALID_CRC           = 0x04;
+	static const uint8_t INVALID_ARGUMENTS     = 0x05;
+	// Never sent, used internally to indicate no status should be
+	// returned.
+	static const uint8_t NO_REPLY              = 0xff;
 };
 
 struct GeneralCallCommands {
-  static const uint8_t RESET = 0x06;
-  static const uint8_t RESET_ADDRESS = 0x04;
+	#if defined(USE_I2C)
+		static const uint8_t RESET = 0x06;
+		static const uint8_t RESET_ADDRESS = 0x04;
+	#elif defined(USE_RS485)
+		// These add 0x40 to the I2C general call commands so they en up
+		// in the 0x40-0x48 "user defined function codes" area.
+		static const uint8_t RESET = 0x46;
+		static const uint8_t RESET_ADDRESS = 0x44;
+	#endif
+};
+
+// Commands typically also supported by application firmware
+struct ProtocolCommands {
+	static const uint8_t GET_PROTOCOL_VERSION  = 0x00;
+	static const uint8_t SET_ADDRESS           = 0x01;
+	static const uint8_t GET_MAX_PACKET_LENGTH = 0x0c;
 };
 
 struct cmd_result {
-  cmd_result(uint8_t status, uint8_t len = 0) : status(status), len(len) {}
-  uint8_t status;
-  uint8_t len;
+	cmd_result(uint8_t status, uint8_t len = 0) : status(status), len(len) {}
+	uint8_t status;
+	uint8_t len;
 };
 
 inline cmd_result cmd_ok(uint8_t len = 0) {
-  return cmd_result(Status::COMMAND_OK, len);
+	return cmd_result(Status::COMMAND_OK, len);
 }
 
 cmd_result processCommand(uint8_t cmd, uint8_t *datain, uint8_t len, uint8_t *dataout, uint8_t maxLen);
+void resetSystem();
